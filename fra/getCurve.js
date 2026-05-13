21a3ddb72e67af6c83b1b959e580f547b9c9140b
NaoNaoMe <naonaome0325@gmail.com>
2026-05-14 05:04:31 +0900

    [FRA] Add Keysight/Agilent oscilloscope support and improve fetch error handling

--- fra/getCurve.js
+++ fra/getCurve.js
@@ -26,6 +26,22 @@ class GetCurve {
     return this.#isBusy;
   }
 
+  ReadList(data) {
+    if (typeof data !== 'string') return [];
+
+    const trimmed = data.trim();
+
+    if (trimmed === '' || trimmed === '+0') return [];
+  
+    const nums = trimmed.split(',').map(Number);
+    if (nums.some(isNaN)) {
+      console.warn('ReadList: invalid numeric data', data);
+      return [];
+    }
+    
+    return nums;
+  }
+
   async open(device) {
     await this.#tmc.open(device);
     this.#device = device;
@@ -46,6 +62,8 @@ class GetCurve {
       this.#device.vendorId == 0x2a8d) {
       // Agilent Technologies, Inc.
       // Keysight Technologies Inc.
+      result = await this.fetch_keysight();
+      result.isSupported = true;
     }
     else if (this.#device.vendorId == 0x0699) {
       // Tektronix, Inc.
@@ -115,6 +133,133 @@ class GetCurve {
     let data_ch2 = [];
     for (let i = 0; i < int8Array.length; i++) {
       data_ch2.push((int8Array[i] - yoff) * ymult);
+    }
+
+    this.#isBusy = false;
+
+    if(data_ch1.length !== data_ch2.length) {
+      return { data_ch1: [], data_ch2: [], length: NaN, rate: NaN };
+    }
+
+    return { data_ch1: data_ch1, data_ch2: data_ch2, length: data_ch1.length, rate: rate };
+  }
+
+  async fetch_keysight() {
+
+    if(this.#isBusy) {
+      return { data_ch1: [], data_ch2: [], length: NaN, rate: NaN };
+    }
+
+    this.#isBusy = true;
+
+    await this.#tmc.write(':STOP');
+    await this.#tmc.delay(100);
+
+    const result = await this.fetch_procedure_keysight();
+
+    await this.#tmc.write(':RUN');
+
+    this.#isBusy = false;
+
+    return { data_ch1: result.data_ch1, data_ch2: result.data_ch2, length: result.data_ch1.length, rate: result.rate };
+  }
+
+
+  async fetch_procedure_keysight() {
+    let tmp;
+    let raw;
+    let buff;
+    let preamble;
+
+    let yoff;
+    let ymult;
+    let yorgn;
+
+    let rate;
+
+    await this.#tmc.write(':WAV:SOUR CHANnel1');
+    await this.#tmc.write(':WAV:SOUR?');
+    tmp = await this.#tmc.read();
+
+    await this.#tmc.write(':WAVeform:PREamble?');
+    tmp = await this.#tmc.read();
+
+    if (tmp === '+0') {
+      return { data_ch1: [], data_ch2: [], length: NaN, rate: NaN };
+    }
+
+    preamble = this.ReadList(tmp);
+
+    let [
+      intFormat,
+      intType,
+      lngPoints,
+      lngCount,
+      dblXIncrement,
+      dblXOrigin,
+      lngXReference,
+      sngYIncrement,
+      sngYOrigin,
+      lngYReference
+    ] = preamble;
+
+    if (intFormat !== 0.0) {
+      await this.#tmc.write(':WAV:FORM BYTE');
+      return { data_ch1: [], data_ch2: [], length: NaN, rate: NaN };
+    }
+
+    rate = 1/dblXIncrement;
+    yoff = lngYReference;
+    ymult =sngYIncrement;
+    yorgn = sngYOrigin;
+
+    await this.#tmc.write(':WAV:DATA?');
+    raw = await this.#tmc.readBlockData();
+    buff = new Uint8Array(raw.buffer);
+
+    let data_ch1 = [];
+    for (let i = 0; i < buff.length; i++) {
+      data_ch1.push(((buff[i] - yoff) * ymult) + yorgn);
+    }
+
+    await this.#tmc.write(':WAV:SOUR CHANnel2');
+    await this.#tmc.write(':WAV:SOUR?');
+    tmp = await this.#tmc.read();
+
+    await this.#tmc.write(':WAVeform:PREamble?');
+    tmp = await this.#tmc.read();
+
+    if (tmp === '+0') {
+      return { data_ch1: [], data_ch2: [], length: NaN, rate: NaN };
+    }
+
+    preamble = this.ReadList(tmp);
+
+    [
+      intFormat,
+      intType,
+      lngPoints,
+      lngCount,
+      dblXIncrement,
+      dblXOrigin,
+      lngXReference,
+      sngYIncrement,
+      sngYOrigin,
+      lngYReference
+    ] = preamble;
+
+    rate = 1/dblXIncrement;
+    yoff = lngYReference;
+    ymult =sngYIncrement;
+    yorgn = sngYOrigin;
+
+    await this.#tmc.write(':WAV:DATA?');
+    raw = await this.#tmc.readBlockData();
+    buff = new Uint8Array(raw.buffer);
+
+    let data_ch2 = [];
+    for (let i = 0; i < buff.length; i++) {
+      data_ch2.push(((buff[i] - yoff) * ymult) + yorgn);
     }
 
     this.#isBusy = false;
